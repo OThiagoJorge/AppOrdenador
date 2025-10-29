d25db8ce0683d773416738e32c13c1e55257b635
thiag_oJorge <thiagojorgedasilva7@gmail.com>
2025-10-29 13:34:57 -0300

    Version 1.1.5 -today's date

--- App.js
+++ App.js
@@ -4,6 +4,18 @@ import { createStaticNavigation } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { CalendarScreen } from './utils/Calendar'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text } from 'react-native'
+
+const LogoTitle = () => {
+    const date = new Date()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
+    const fullDate = `${day}/${month}/${year}`
+    return (
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>{fullDate}</Text>
+    )
+}
 
 export const HomeScreen = () => {
     return (
@@ -16,7 +28,12 @@ export const HomeScreen = () => {
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Home',
   screens: {
-    Home: HomeScreen,
+    Home: {
+      screen: HomeScreen,
+      options: {
+        headerTitle: (props) => <LogoTitle {...props} />,
+      },
+    },
     Calendar: CalendarScreen
   }
 })
--- libs/Notifications.js
+++ libs/Notifications.js
@@ -1,40 +1,40 @@
-import { useState, useEffect } from 'react';
-import { Pressable, Text } from 'react-native';
-import * as Notifications from 'expo-notifications';
-import { registerForPushNotificationsAsync, sendPushNotification } from './NotificationsConfig'
-import { styles } from '../Styles';
+// import { useState, useEffect } from 'react';
+// import { Pressable, Text } from 'react-native';
+// import * as Notifications from 'expo-notifications';
+// import { registerForPushNotificationsAsync, sendPushNotification } from './NotificationsConfig'
+// import { styles } from '../Styles';
 
-export const MyPushNotifications = () => {
-    const [expoPushToken, setExpoPushToken] = useState('');
-    const [notification, setNotification] = useState(undefined);
+// export const MyPushNotifications = () => {
+//     const [expoPushToken, setExpoPushToken] = useState('');
+//     const [notification, setNotification] = useState(undefined);
 
-    useEffect(() => {
-        registerForPushNotificationsAsync()
-            .then(token => setExpoPushToken(token ?? ''))
-            .catch(error => setExpoPushToken(`${error}`));
+//     useEffect(() => {
+//         registerForPushNotificationsAsync()
+//             .then(token => setExpoPushToken(token ?? ''))
+//             .catch(error => setExpoPushToken(`${error}`));
 
-        const notificationListener = Notifications.addNotificationReceivedListener(notification => {
-            setNotification(notification);
-        });
+//         const notificationListener = Notifications.addNotificationReceivedListener(notification => {
+//             setNotification(notification);
+//         });
 
-        const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
-            console.log(response);
-        });
+//         const responseListener = Notifications.addNotificationResponseReceivedListener(response => {
+//             console.log(response);
+//         });
 
-        return () => {
-            notificationListener.remove();
-            responseListener.remove();
-        };
-    }, []);
+//         return () => {
+//             notificationListener.remove();
+//             responseListener.remove();
+//         };
+//     }, []);
 
-    return (
-        <Pressable
-            style={styles.button}
-            onPress={async () => {
-                await sendPushNotification(expoPushToken);
-            }}
-        >
-            <Text style={styles.text}>Notificar</Text>
-        </Pressable>
-    );
-}
+//     return (
+//         <Pressable
+//             style={styles.button}
+//             onPress={async () => {
+//                 await sendPushNotification(expoPushToken);
+//             }}
+//         >
+//             <Text style={styles.text}>Notificar</Text>
+//         </Pressable>
+//     );
+// }
