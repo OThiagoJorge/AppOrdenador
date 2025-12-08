c35a5c9886424fee804e67bcf49a388a42d24c96
thiag_oJorge <thiagojorgedasilva7@gmail.com>
2025-12-08 18:58:52 -0300

    Version 1.1.7 -changing date part 2

--- App.js
+++ App.js
@@ -7,31 +7,42 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { Pressable, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { View } from 'react-native'
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons'
 
 const LogoTitle = () => {
-    const todaysDate = new Date()
-    const [date, setDate] = useState(todaysDate.getTime())
+    const [date, setDate] = useState(new Date())
+
+    const formatDate = (d) => {
+      const day = String(d.getDate()).padStart(2, "0")
+      const month = String(d.getMonth() + 1).padStart(2, "0")
+      const year = d.getFullYear()
+      return `${day}/${month}/${year}`
+    }
 
     return (
         <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
           <Pressable onPress={() => {
-            setDate(date - 86400000)
-            alert(date)
-          }}>
-            <Text 
-              style={{fontWeight: 'bold',fontSize: 24, color: 'black'}}> 
-              {'<'} 
-            </Text>
-          </Pressable>
-            <Text style={{fontSize: 20, fontWeight: 'bold'}}>15/11/2025</Text>
-          <Pressable onPress={() => {
-            setDate(date + 86400000)
-            alert(date)
+            const newDate = new Date(date)
+            newDate.setDate(newDate.getDate() - 1)
+            setDate(newDate)
           }}>
             <Text 
               style={{fontWeight: 'bold',fontSize: 24, color: 'black'}}
+            > 
+              <SimpleLineIcons name="arrow-left-circle" size={24} color="black" />
+            </Text>
+          </Pressable>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>{formatDate(date)}</Text>
+          <Pressable onPress={() => {
+            const newDate = new Date(date)
+            newDate.setDate(newDate.getDate() + 1)
+            setDate(newDate)
+          }
+          }>
+            <Text 
+              style={{fontWeight: 'bold',fontSize: 24, color: 'black'}}
             >
-              {'>'} 
+              <SimpleLineIcons name="arrow-right-circle" size={24} color="black" /> 
             </Text>
           </Pressable>
         </View>
