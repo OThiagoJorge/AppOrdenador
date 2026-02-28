4e8a15de72f7616a876a5a74d6cdedcbbe942a54
thiag_oJorge <thiagojorgedasilva7@gmail.com>
2026-02-28 14:56:19 -0300

    Version 1.5.5 -There's an additional step to send modifications on title and description

--- utils/TaskUtils/TaskDescription.js
+++ utils/TaskUtils/TaskDescription.js
@@ -1,4 +1,4 @@
-import { Pressable, TextInput, Text } from 'react-native'
+import { Pressable, TextInput, Text, Modal, View } from 'react-native'
 import { styles } from '@/Styles'
 import { useState, useContext } from 'react'
 import { GlobalContext } from '@/resources/Context'
@@ -7,36 +7,69 @@ import { updateTask } from '@/resources/database'
 export const TaskDescription = ({task, descriptionInputIsVisible, setDescriptionInputIsVisible, onChangeText}) => {
 
     const [description, setDescription] = useState(task.description)
+    const [modalVisible, setModalVisible] = useState(false)
 
     const {taskDidUpdate, setTaskDidUpdate} = useContext(GlobalContext)
 
     return (
-        <Pressable 
-            style={{
-                marginLeft: 60,
-                width: '70%',
-                backgroundColor: '#0a7e8c'
-            }}
-            onLongPress={() => setDescriptionInputIsVisible(!descriptionInputIsVisible)}
-        >
-            <TextInput
-                style={[styles.input, {color: 'white'}]}
-                onChangeText={(text) => setDescription(text)}
-                value={description}
-                placeholder="Descrição"
-                editable={descriptionInputIsVisible}
-                multiline={true}
-            />
-            {descriptionInputIsVisible &&
-                <Pressable
-                    onPress={() => {
-                        updateTask(task.id, {description: description})
-                        setTaskDidUpdate(!taskDidUpdate)
-                    }}
-                >
-                    <Text>Enviar</Text>
-                </Pressable>
-    }
-        </Pressable>
+        <View>
+            <Pressable 
+                style={{
+                    marginLeft: 60,
+                    width: '70%',
+                    backgroundColor: '#0a7e8c'
+                }}
+                onLongPress={() => setDescriptionInputIsVisible(!descriptionInputIsVisible)}
+            >
+                <TextInput
+                    style={[styles.input, {color: 'white'}]}
+                    onChangeText={(text) => setDescription(text)}
+                    value={description}
+                    placeholder="Descrição"
+                    editable={descriptionInputIsVisible}
+                    multiline={true}
+                />
+                {descriptionInputIsVisible &&
+                    <Pressable
+                        onPress={() => {
+                            setModalVisible(true)
+                        }}
+                    >
+                        <Text>Enviar</Text>
+                    </Pressable>
+                }
+            </Pressable>
+            <Modal
+                style={{marginTop: 100}}
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.')
+                    setModalVisible(!modalVisible)
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Alterar Descrição?</Text>
+                        <Pressable
+                            onPress={() => {
+                                updateTask(task.id, {description: description})
+                                setTaskDidUpdate(!taskDidUpdate)
+                                setModalVisible(false)
+                            }}
+                        >
+                            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Sim</Text>
+                        </Pressable>
+                        <Pressable
+                            style={[styles.button, styles.buttonClose]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={styles.textStyle}>fechar</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+        </View>
     )
 }
--- utils/TaskUtils/components/TaskTitle.js
+++ utils/TaskUtils/components/TaskTitle.js
@@ -1,44 +1,78 @@
 import { updateTask } from '@/resources/database'
-import { TextInput, Pressable, Text } from 'react-native'
+import { TextInput, Pressable, Text, Alert, View, Modal } from 'react-native'
 import { useState, useContext } from 'react'
 import { GlobalContext } from '@/resources/Context'
+import { styles } from '@/Styles'
 
 export const TaskTitle = ({ task, taskTitleInputIsVisible, setTaskTitleInputIsVisible, onChangeText, inListMode }) => {
 
     const [title, setTitle] = useState(task.title)
+    const [modalVisible, setModalVisible] = useState(false)
 
     const {taskDidUpdate, setTaskDidUpdate} = useContext(GlobalContext)
 
     return (
-        <Pressable 
-            style={{
-                marginLeft: inListMode ? 20 : 60,
-                width: inListMode ? '100%' : '70%'
-            }}
-            onLongPress={() => setTaskTitleInputIsVisible(!taskTitleInputIsVisible)}
-        >            
-            <TextInput
+        <View>
+            <Pressable 
                 style={{
-                    fontSize: 20,
-                    backgroundColor: inListMode ? 'transparent' : 'yellow'
+                    marginLeft: inListMode ? 20 : 60,
+                    width: inListMode ? '100%' : '70%'
                 }}
-                // Corrigir bug que impede o texto de ser alterado
-                value={title}
-                placeholder={task.title}
-                editable={taskTitleInputIsVisible}
-                multiline={true}
-                onChangeText={(text) => setTitle(text)}
-            />
-            {taskTitleInputIsVisible && 
-                <Pressable
-                    onPress={() => {
-                        updateTask(task.id, title)
-                        setTaskDidUpdate(!taskDidUpdate)
+                onLongPress={() => setTaskTitleInputIsVisible(!taskTitleInputIsVisible)}
+            >            
+                <TextInput
+                    style={{
+                        fontSize: 20,
+                        backgroundColor: inListMode || taskTitleInputIsVisible ? 'gray' : 'yellow',
+                        color: inListMode || taskTitleInputIsVisible ? 'white' : 'black'
                     }}
-                >
-                    <Text>Enviar</Text>
-                </Pressable>
-    }
-        </Pressable>
+                    value={title}
+                    placeholder={task.title}
+                    editable={taskTitleInputIsVisible}
+                    multiline={true}
+                    onChangeText={(text) => setTitle(text)}
+                />
+                {taskTitleInputIsVisible && 
+                    <Pressable
+                        onPress={() => {
+                            setModalVisible(true)
+                        }}
+                    >
+                        <Text>Enviar</Text>
+                    </Pressable>
+                }
+            </Pressable>
+            <Modal
+                style={{marginTop: 100}}
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.')
+                    setModalVisible(!modalVisible)
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Alterar título?</Text>
+                        <Pressable
+                            onPress={() => {
+                                updateTask(task.id, title)
+                                setTaskDidUpdate(!taskDidUpdate)
+                                setModalVisible(false)
+                            }}
+                        >
+                            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Sim</Text>
+                        </Pressable>
+                        <Pressable
+                            style={[styles.button, styles.buttonClose]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={styles.textStyle}>fechar</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+        </View>
     )
 }
